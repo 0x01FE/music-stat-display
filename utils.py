2740436a6278d8b5472362becf251a1e24a204e3
0x01FE <0x01FE@0x01FE.net>
2023-09-18 09:48:17 -0500

    fixed error in utils

--- utils.py
+++ utils.py
@@ -7,18 +7,18 @@ def calculate_date_range(year : int, month : int) -> tuple[datetime.datetime, da
     last_day = calendar.monthrange(year, month)[1]
 
     if month < 10:
-        end = datetime.strptime(f"{year}-0{month}-{last_day}", "%Y-%m-%d")
+        end = datetime.datetime.strptime(f"{year}-0{month}-{last_day}", "%Y-%m-%d")
     else:
-        end = datetime.strptime(f"{year}-{month}-{last_day}", "%Y-%m-%d")
+        end = datetime.datetime.strptime(f"{year}-{month}-{last_day}", "%Y-%m-%d")
 
-    start = datetime.strptime(f"{year}-{month}", "%Y-%m")
+    start = datetime.datetime.strptime(f"{year}-{month}", "%Y-%m")
 
     return (start, end)
 
 
 def valid_month(year : int, month : int) -> bool:
 
-    today = datetime.now()
+    today = datetime.datetime.now()
 
     if month > today.month or month < 1 or year > today.year or year < 2023:
         return False
